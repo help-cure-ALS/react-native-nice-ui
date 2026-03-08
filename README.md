ae00f2bb322b9cb5d1ecfde8796c7babfdce3109
Harald Hanek <hanek@delacap.com>
2026-03-08 17:48:49 +0100

    v1.3.3: iOS-style divider inset for leftCmp

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ A reusable UI component library for React Native with theming support, designed 
 ## Installation
 
 ```bash
-npm install github:help-cure-ALS/react-native-nice-ui#v1.3.2
+npm install github:help-cure-ALS/react-native-nice-ui#v1.3.3
 ```
 
 ### Peer Dependencies
--- dist/theme/tokens.defaults.js
+++ dist/theme/tokens.defaults.js
@@ -46,7 +46,7 @@ export const defaultTokens = {
     listItemMarginLeft: isIOSVersionOrHigher(26) ? 14 : (Platform.OS === 'ios' ? 18 : 20),
     listItemRadius: isIOSVersionOrHigher(26) ? 14 : 10,
     listSectionMarginTop: 30,
-    listSectionPaddingHorizontal: isIOSVersionOrHigher(26) ? 20 : 13,
+    listSectionPaddingHorizontal: isIOSVersionOrHigher(26) ? 16 : 13,
     listSectionRadius: isIOSVersionOrHigher(26) ? 16 : 10,
     listSpacedGap: 10,
     listItemDividerMarginRight: isIOSVersionOrHigher(26) ? 14 : 0
